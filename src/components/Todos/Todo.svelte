5fd0b4d2234dbdbea7b262936a1a0d7a0ac71593
hardyyb2 <hardikb3732@gmail.com>
2021-08-25 21:04:28 +0530

    [REMOVED] yarn 3

--- src/components/Todos/Todo.svelte
+++ src/components/Todos/Todo.svelte
@@ -1,7 +1,23 @@
 <script lang="typescript">
+  import { createEventDispatcher } from "svelte";
+
   export let name: string;
 </script>
 
-<div style="border: 1px solid red;">
-  {name}
+<div class="todo">
+  <span>{name}</span>
+  <span on:click={() => dispa}>&Cross;</span>
 </div>
+
+<style>
+  .todo {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 20px;
+    border: 1px solid #efefef;
+  }
+
+  .todo > span:last-child {
+    cursor: pointer;
+  }
+</style>
